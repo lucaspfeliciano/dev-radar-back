a2f2cf97e7d5b6aca49bb29ad72de4e931160716
Lucas Feliciano <lucas.feliciano@bry.com.br>
2020-01-31 16:42:33 -0200

    Adiciona configurações para atualização em tempo real no mobile

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 const express =  require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors')
+const http = require('http')
+const { setupWebsocket } = require('./websocket')
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://lucasfeliciano:lucas123@cluster0-uax4l.mongodb.net/semana10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
